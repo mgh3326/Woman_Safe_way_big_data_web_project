6b582b6f4f72223e86d4db5071885edb213975c6
mgh3326 <mgh3326@naver.com>
2018-02-15 14:11:25 +0900

    wether update and police init

--- latitude_longitude/lat_log.py
+++ latitude_longitude/lat_log.py
@@ -3,17 +3,20 @@ import json
 from urllib.request import urlopen
 from urllib import parse
 
-location = "미아동"
-print(parse.quote(location))
-data = urllib.request.urlopen(
-    "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&language=ko&address=" + parse.quote(location))
 
-json = json.loads(data.read())
+def func(str):
+    location = str
+    print("http://maps.googleapis.com/maps/api/geocode/json?sensor=false&language=ko&address="+parse.quote(location))
+    data = urllib.request.urlopen(
+        "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&language=ko&address=" + parse.quote(location))
 
-latitude = json["results"][0]["geometry"]["location"]["lat"]
-longitude = json["results"][0]["geometry"]["location"]["lng"]
+    _json = json.loads(data.read())
 
-print(latitude)
-print(longitude)
+    latitude = _json["results"][0]["geometry"]["location"]["lat"]
+    longitude = _json["results"][0]["geometry"]["location"]["lng"]
+
+    print(latitude)
+    print(longitude)
 
 # 출처: http://wkdgusdn3.tistory.com/entry/Python-주소로-위도-경도-검색하기 [착한코딩님의 블로그]
+func("서울 파출소")
