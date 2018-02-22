e25136f243406edec53e9e39c82a77f12556180f
mgh3326 <mgh3326@naver.com>
2018-02-22 14:43:50 +0900

    file_input update

--- aram/file_input.py
+++ aram/file_input.py
@@ -1,14 +1,33 @@
-oh=[]
-f = open("일별 기록.csv", 'r')
-line = f.readline()
+day = []
+f = open("day_2016.csv", 'r')  # 날짜 파일 입력
+line = f.readline()  # head 부분 머리기 위함
 
 while True:
     line = f.readline()
-    if not line: break
-    oh.append(line)
+    if not line:
+        break
+    day.append(str(line).split('\n')[0])  # 공백 제거를 위함
 f.close()
-for index in oh:
-    temp = str(index).split(',')[0]
-    print(temp+"  : ",end="")
-    print(temp.split('-')[2])
-    
+data = []
+fr = open("일별 기록.csv", "r")  # 기록 파일 입력
+fw = open("day_2016_result.csv", 'w')  # 결과 파일 출력을 위한 입력
+
+line = fr.readline()  # head 부분 머리기 위함
+while True:
+    line = fr.readline()
+    if not line:
+        break
+    for index in day:
+        if index == str(line).split(',')[0]:
+            print(str(line).split('\n')[0])
+        else:
+            print(index)
+    data.append(str(line).split(',')[0])  # 공백 제거를 위함
+fr.close()
+# print(data)
+# print(day)
+# for index in day:
+#     print(index)
+
+fw.close()
+fr.close()
