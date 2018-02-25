31f368ccbdf43a6dc7a9bb761a03c2ed8f7a2e8a
mgh3326 <mgh3326@naver.com>
2018-02-25 11:40:04 +0900

    update

--- 데이터통합/file_input.py
+++ 데이터통합/file_input.py
@@ -1,14 +1,14 @@
-oh=[]
+oh = []
 f = open("일별 기록.csv", 'r')
 line = f.readline()
 
 while True:
     line = f.readline()
-    if not line: break
+    if not line:
+        break
     oh.append(line)
 f.close()
 for index in oh:
     temp = str(index).split(',')[0]
-    print(temp+"  : ",end="")
+    print(temp+"  : ", end="")
     print(temp.split('-')[2])
-    
