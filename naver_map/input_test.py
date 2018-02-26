30a13f035183e173660a8b2c2b594352921d9042
mgh3326 <mgh3326@naver.com>
2018-02-26 09:13:31 +0900

    input update

---
+++ naver_map/input_test.py
@@ -0,0 +1,11 @@
+f = open("경도위도지하철입력용.csv", 'r')  # 날짜 파일 입력
+line = f.readline()
+output = ""
+output += "위치,경도,위도,구"
+while True:
+    line = f.readline()
+    if not line:
+        break
+    print(str(line).split(',')[1]+str(line).split(',')[2].split('\n')[0])
+    # oh.append(str(line).split('\n')[0])  # 공백 제거를 위함
+f.close()
