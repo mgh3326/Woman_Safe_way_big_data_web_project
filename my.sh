4feb79b10ff4e2526d820ba63a2605d136dab2cd
mgh3326 <mgh3326@naver.com>
2018-03-04 21:10:58 +0900

    auto git update

--- my.sh
+++ my.sh
@@ -1,4 +1,20 @@
+#!/bin/bash
 git pull origin master
 git add *
-git commit -m "update"
+if [ $# -eq 1 ]
+then # 매개 변수가 한개 일때 (띄어쓰기가 없는 경우)
+
+    git commit -m "$1"
+
+elif [ $# -gt 1 ] # 띄어쓰기 있는 경우
+then
+        for a
+    do
+        # echo $a
+        oh=$oh" "$a
+    done
+    git commit -m "$oh"
+else #매개 변수가 없는 경우 update 내용으로 commit 되도록 하였습니다.
+    git commit -m "update"
+fi
 git push origin master
